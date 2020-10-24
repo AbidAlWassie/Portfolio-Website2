aaa0fd2b243b2cd66a8de7f1b13ff9068fd13ece
Abid Al-Wasi <abidalwasi@outlook.com>
2020-10-24 11:42:52 +0600

    updated alongside everything

---
+++ js/script.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  const config = {
+    type: "carousel",
+    perView: 1,
+    breakpoints: {
+      // 1024: {
+      //   perView: 1
+      // },
+      // 600: {
+      //   perView: 1
+      // }
+    }
+  }
+  new Glide(".glide", config).mount();
+  
+  
+  const glideContent = document.querySelector(".glide-content");
+  glideContent.addEventListener("click", function() {
+    console.log("Hello");
+  });
+}
