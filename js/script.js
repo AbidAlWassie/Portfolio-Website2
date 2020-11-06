b7b9fbc4fbd1f408665dd42aea5560a1a30c8d70
Abid Al-Wasi <abidalwasi@outlook.com>
2020-11-06 06:05:23 +0600

    updated readme.md

--- js/script.js
+++ js/script.js
@@ -1,21 +1,78 @@
 window.onload = function() {
-  const config = {
-    type: "carousel",
-    perView: 1,
-    breakpoints: {
-      // 1024: {
-      //   perView: 1
-      // },
-      // 600: {
-      //   perView: 1
-      // }
-    }
-  }
   
-  new Glide(".glide", config).mount();
+  $(document).ready(function(){
+
+    var scrollWindow = function() {
+      $(window).scroll(function(){
+        var $w = $(this),
+          st = $w.scrollTop(),
+          navbar = $(".navbar"),
+          sd = $(".js-scroll-wrap");
+        navLink = $(".nav-link");
+        navbarExtended = $(".navbar-nav");
+        brandOtherLetters = $(".otherLetters");
+        btnLine = $(".btn-line");
+
+        if (st > 150) {
+          if ( !navbar.hasClass("scrolled") ) {
+            navbar.addClass("scrolled");
+            navLink.addClass("nav-link-dark");
+            navbarExtended.addClass("navbar-nav-dark");
+            brandOtherLetters.addClass("brandLettersDark");
+            btnLine.addClass("btn-line-dark");
+          }
+        }
+        if (st < 150) {
+          if ( navbar.hasClass("scrolled") ) {
+            navbar.removeClass("scrolled sleep");
+            navLink.removeClass("nav-link-dark");
+            navbarExtended.removeClass("navbar-nav-dark");
+            brandOtherLetters.removeClass("brandLettersDark");
+            btnLine.removeClass("btn-line-dark");
+          }
+        }
+        if ( st > 350 ) {
+          if ( !navbar.hasClass("awake") ) {
+            navbar.addClass("awake");
+          }
+
+          if(sd.length > 0) {
+            sd.addClass("sleep");
+          }
+        }
+        if ( st < 350 ) {
+          if ( navbar.hasClass("awake") ) {
+            navbar.removeClass("awake");
+            navbar.addClass("sleep");
+          }
+          if(sd.length > 0) {
+            sd.removeClass("sleep");
+          }
+        }
+      });
+    };
+    scrollWindow();
+
+
+    // var anchor = $(".nav-link");
+    // anchor.on('click', function(event) {
+    //
+    //   if (this.hash !== "") {
+    //     event.preventDefault();
+    //
+    //     var hash = this.hash;
+    //
+    //     var target = $(hash).offset().top-150;
+    //     $('html, body').animate({
+    //       scrollTop: target
+    //     }, 500, function(){
+    //       // window.location.hash = hash;
+    //     });
+    //   }
+    // });
+
+    var context;
+    var $window = $(window);
   
-  const glideContent = document.querySelector(".glide-content");
-  glideContent.addEventListener("click", function() {
-    console.log("Hello");
   });
 }
